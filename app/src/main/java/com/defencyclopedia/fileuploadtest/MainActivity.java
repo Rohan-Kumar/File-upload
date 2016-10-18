b0224448b2191b6286e30e9fa7db3bde70c06b7a
Rohan Kumar <rohan.carmel@gmail.com>
2016-10-19 01:28:29 +0530

    reading response

--- app/src/main/java/com/defencyclopedia/fileuploadtest/MainActivity.java
+++ app/src/main/java/com/defencyclopedia/fileuploadtest/MainActivity.java
@@ -2,9 +2,7 @@ package com.defencyclopedia.fileuploadtest;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -47,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -56,28 +55,31 @@ public class MainActivity extends AppCompatActivity {
             upload(new File(filePath));
         }
     }
+
     private void upload(File myFile) {
         AsyncHttpClient client = new AsyncHttpClient();
 
         RequestParams params = new RequestParams();
         try {
-            params.put("file", myFile);
+            params.put("filename", myFile);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
 
 //        client.get("http://192.168.0.102/cgi-bin/python.py?first_name=Rohan&last_name=Kumar",/*params,*/ new AsyncHttpResponseHandler() {
-        client.post("http://192.168.0.102/cgi-bin/save_file.py",params, new AsyncHttpResponseHandler() {
+        client.post("http://192.168.0.102/cgi-bin/save_file.py", params, new AsyncHttpResponseHandler() {
 
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
                 Toast.makeText(MainActivity.this, "Uploaded Successfully!", Toast.LENGTH_SHORT).show();
+                Log.d("Tag", "response: " + new String(responseBody));
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-                Toast.makeText(MainActivity.this, "Failed! "+statusCode, Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Failed! " + statusCode, Toast.LENGTH_SHORT).show();
                 error.printStackTrace();
+                Log.d("TAG", "onFailure: "+new String(responseBody));
             }
 
         });
